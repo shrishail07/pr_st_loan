0664ba33580c6595137e7309f9baf06f21218480
shrishail07 <144917838+shrishail07@users.noreply.github.com>
2026-09-05 15:32:07 +0530

    Update 1_Alumni_Investor.py

--- pages/1_Alumni_Investor.py
+++ pages/1_Alumni_Investor.py
@@ -27,12 +27,12 @@ st.markdown("""
             border: none !important;
         }
         
-        /* 2. Target text color inside the form to be black/dark for readability */
+        /* 2. Target text color inside the form to be Pink/dark for readability */
         div[data-testid="stForm"] p, 
         div[data-testid="stForm"] h3, 
         div[data-testid="stForm"] label,
         div[data-testid="stForm"] span {
-            color: #000000 !important; /* Black Text */
+            color: #C71585 !important; /* Pink Text */
             font-weight: bold !important;
         }
 
