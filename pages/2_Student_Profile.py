ad8e477fc371b54b9c3dac43fcb412d01a8e2c91
shrishail07 <144917838+shrishail07@users.noreply.github.com>
2026-09-05 15:13:40 +0530

    Update prompt for user details in student profile

--- pages/2_Student_Profile.py
+++ pages/2_Student_Profile.py
@@ -3,7 +3,7 @@ from supabase_client import supabase
 st.markdown(
     '<div style="background-color: #e8f4f8; padding: 10px; border-left: 5px solid #29b6f6; border-radius: 4px;">'
     '<span style="font-size: 20px; font-weight: bold; color: #005a80;">'
-    'Please fill the Head of the Department Details.....'
+    'Please fill Your Details For Loan Approval.....'
     '</span>'
     '</div>', 
     unsafe_allow_html=True
